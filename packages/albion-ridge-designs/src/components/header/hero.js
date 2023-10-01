2c7af57a7a0cee7d2bff3bf7745ddbaa083785c2
Rachel Moore <rachel@rachelmoore.net>
2023-09-30 21:48:56 -0700

    small screen hero

--- packages/albion-ridge-designs/src/components/header/hero.js
+++ packages/albion-ridge-designs/src/components/header/hero.js
@@ -5,7 +5,8 @@ import {
     Heading,
     HStack,
     Flex,
-    Button
+    Button,
+    useMediaQuery
   } from "@chakra-ui/react";
 import TextTransition, { presets } from 'react-text-transition';
 import useSticky from "../../hooks/useSticky";
@@ -14,6 +15,7 @@ function Hero({ image, video, headingAnimationList, headingTop, headingBottom, c
   const [index, setIndex] = useState(0);
   const { element } = useSticky();
   const [textsArr, setTextsArr] = useState([]);
+  const [isSmallerThan420] = useMediaQuery('(max-width: 420px)')
   const isFontLoaded = useFontFaceObserver([
     { family: 'Amalta' }, // Same name you have in your CSS
   ]);
@@ -63,9 +65,10 @@ function Hero({ image, video, headingAnimationList, headingTop, headingBottom, c
           {isFontLoaded && textsArr.length > 0 &&
             <Flex direction="column" height="100vh" justifyContent="center" alignItems="center">
 
-                <Flex direction="column" justifyContent="center" alignItems="center" border="5px solid black" borderRadius="30px" bg="brand.900" opacity="80%" pt={5} pb={5} minWidth={{base: "375px", sm: "475px", md: "500px"}}>
+                <Flex direction="column" justifyContent="center" alignItems="center" border="5px solid black" borderRadius="30px" bg="brand.900" opacity="80%" pt={5} pb={5} minWidth={{base: "325px", sm: "475px", md: "500px"}}>
           
                     <HStack>
+                      {!isSmallerThan420 &&
                       <HeroHeading>
                         <Heading color="brand.800" size="3xl" fontFamily="Amalta">{headingTop} </Heading>
                         <Heading color="brand.500" size="3xl" fontFamily="Amalta" p={5} textAlign="center">
@@ -77,6 +80,20 @@ function Hero({ image, video, headingAnimationList, headingTop, headingBottom, c
                         </Heading>
                         <Heading color="brand.800" size="3xl" fontFamily="Amalta"> {headingBottom} </Heading>
                       </HeroHeading>
+                      }
+                      {isSmallerThan420 &&
+                      <HeroHeadingMobile>
+                        <Heading color="brand.800" size="2xl" fontFamily="Amalta">{headingTop} </Heading>
+                        <Heading color="brand.500" size="2xl" fontFamily="Amalta" p={2} textAlign="center">
+                          <TextTransition
+                            text={ textsArr[index % textsArr.length] }
+                            springConfig={presets.wobbly} // default, gentle, wobbly, stiff, slow, molasses
+                            style={{width: "100%", textAlign: "center", display: "flex", alignItems: "center", justifyContent: "center"}}
+                          />
+                        </Heading>
+                        <Heading color="brand.800" size="2xl" fontFamily="Amalta"> {headingBottom} </Heading>
+                      </HeroHeadingMobile>
+                      }
                     </HStack>
           
                 </Flex>
@@ -109,10 +126,16 @@ const Welcome = styled.section`
     text-align: center;
 }
 `
-const HeroHeading = styled.span`
+const HeroHeading = styled.div`
     -webkit-text-stroke-width: 1px;
     -webkit-text-stroke-color: black;
     padding: .5em;
+`
+
+const HeroHeadingMobile = styled.div`
+    -webkit-text-stroke-width: .5px;
+    -webkit-text-stroke-color: black;
+    padding: .25em;
 `
 
 const HeroSubheading = styled.span`
