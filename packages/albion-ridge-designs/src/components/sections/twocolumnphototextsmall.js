1916b167487f69ce78c5fed1f840566c6c8ca794
Rachel Moore <rachel@rachelmoore.net>
2023-10-04 21:27:09 -0700

    small component html option

--- packages/albion-ridge-designs/src/components/sections/twocolumnphototextsmall.js
+++ packages/albion-ridge-designs/src/components/sections/twocolumnphototextsmall.js
@@ -11,7 +11,7 @@ import {
     useMediaQuery
   } from "@chakra-ui/react";
   
-  function TwoColumnPhotoTextSmall({ backgroundColor, buttonLink, buttonText, heading, subheading, headingFont, subheadingFont, image, imageSide, text, textColor, height, spacing, libraries }) {
+  function TwoColumnPhotoTextSmall({ backgroundColor, buttonLink, buttonText, heading, subheading, headingFont, subheadingFont, image, imageSide, text, textColor, height, html, spacing, libraries }) {
     const Html2React = libraries.html2react.Component;  
     const [finalHeight, setFinalHeight] = useState();
     const [isSmallerThan480] = useMediaQuery('(max-width:480px)');
@@ -48,9 +48,14 @@ import {
                             {subheading &&
                             <Heading size="md" color={textColor} fontFamily={subheadingFont} fontWeight="500" mt={{base: 1, md: 2}} mb={4}>{subheading}</Heading>
                             }
-                            {text &&
+                            {text && !html &&
                             <Text color={textColor} textAlign="justify" className="page-text">
                                 <Html2React html={text} />
+                            </Text>
+                            }
+                            {html &&
+                            <Text color={textColor} textAlign="justify" className="page-text">
+                                <Html2React html={html} />
                             </Text>
                             }
                             {buttonText &&
@@ -86,9 +91,14 @@ import {
                             {subheading &&
                             <Heading size="md" color={textColor} fontFamily={subheadingFont} fontWeight="500" mt={{base: 1, md: 2}} mb={4}>{subheading}</Heading>
                             }
-                            {text &&
+                            {text && !html &&
                             <Text color={textColor} textAlign="justify" className="page-text">
                                 <Html2React html={text} />
+                            </Text>
+                            }
+                            {html &&
+                            <Text color={textColor} textAlign="justify" className="page-text">
+                                <Html2React html={html} />
                             </Text>
                             }
                             {buttonText &&
